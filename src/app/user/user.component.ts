323a48ce5a9c9875ac4429c7748221446d96622e
s20513 <s20513@pjwstk.edu.pl>
2025-03-01 13:09:27 +0100

    Added input parameters to User component. Added outputting value from User to App component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, Input, input, computed, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true })  id!: string;
+  @Input({ required: true })  avatar!: string;
+  @Input({ required: true })  name!: string;
+  @Output() select = new EventEmitter(); 
+
+  get imagePath(){
+    return 'users/' + this.avatar;
+  }
+
+  onSelectUser(){
+    this.select.emit(this.id);
+  }
+}
+
+/* Component parameters with signals
+
+import { Component, Input, input, computed } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  // Version without input signals
+  //@Input({ required: true })  avatar!: string;
+  //@Input({ required: true })  name!: string;
+
+  //Version with input signals
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return 'users/' + this.avatar();
+  })
+
+  onSelectUser(){
+    
+  }
+}
+*/
+
+
+
+/*
+import { Component, signal, computed } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -11,13 +66,26 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'users/' + this.selectedUser().avatar)
+
 
   get imagePath(){
-    return 'users/' + this.selectedUser.avatar;
+    return 'users/' + this.selectedUser().avatar;
   }
 
   onSelectUser(){
-    console.log("Clicked!");
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
+
   }
+
+  // Version without signal
+  //onSelectUser(){
+    //const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    //this.selectedUser = DUMMY_USERS[randomIndex];
+
+  //}
 }
+
+*/

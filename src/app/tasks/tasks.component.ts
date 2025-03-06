37d4bafba310cc13bdeb2635e3b3a5e23e92603e
s20513 <s20513@pjwstk.edu.pl>
2025-03-06 18:22:09 +0100

    Added new-task component. Added displaying selected user's tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,18 +1,57 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent]
+  imports: [TaskComponent, NewTaskComponent]
 })
 export class TasksComponent {
-  @Input()  name?: string;
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) userName!: string;
 
+  isAddingTask = false;
 
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary:
+        'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Build a first prototype of the online shop website',
+      dueDate: '2024-05-31',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary:
+        'Prepare and describe an issue template which will help with project management',
+      dueDate: '2024-06-15',
+    },
+  ]
 
+  get selectedUserTasks(){
+    return this.tasks.filter(e => e.userId === this.userId);
+  }
   
+  onCompleteTask(id: string){
+    this.tasks = this.tasks.filter(e => e.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
+  }
+
 }

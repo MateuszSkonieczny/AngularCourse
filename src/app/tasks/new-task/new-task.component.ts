33b3cc22945d78b7e37866ac64646c09c026b525
s20513 <s20513@pjwstk.edu.pl>
2025-03-08 15:28:54 +0100

    Added adding new task do tasks list

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
@@ -9,11 +10,25 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   @Output() cancle = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
   enteredTitle = "";
+
+  /* Version with signals does not require any change in the html code
+  You still use ngModel. */
+  //enteredTitle = signal("");
+
   enteredSummary = "";
   eneteredDate = "";
 
   onCancle(){
     this.cancle.emit();
+  }
+
+  onSubmit(){
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.eneteredDate
+    });
   }
 }

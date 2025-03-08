3139cdfdd34e894d13070bfe5e685f9a0bb03762
s20513 <s20513@pjwstk.edu.pl>
2025-03-08 14:56:15 +0100

    Added body and styles to new-task component. Added closing new-task dialog

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+  @Output() cancle = new EventEmitter<void>();
+  enteredTitle = "";
+  enteredSummary = "";
+  eneteredDate = "";
 
+  onCancle(){
+    this.cancle.emit();
+  }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -54,4 +54,7 @@ export class TasksComponent {
     this.isAddingTask = true;
   }
 
+  onCancleAddTask(){
+    this.isAddingTask = false;
+  }
 }

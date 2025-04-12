64c976788493a05df2f4ad78b80c2a95768b6669
s20513 <s20513@pjwstk.edu.pl>
2025-04-12 15:10:19 +0200

    Added module 12 project

---
+++ Module_12/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -0,0 +1,59 @@
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { Place } from '../place.model';
+import { PlacesComponent } from '../places.component';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesService } from '../places.service';
+
+@Component({
+  selector: 'app-available-places',
+  standalone: true,
+  templateUrl: './available-places.component.html',
+  styleUrl: './available-places.component.css',
+  imports: [PlacesComponent, PlacesContainerComponent],
+})
+export class AvailablePlacesComponent implements OnInit {
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  httpError = signal("");
+
+  private placesService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(){
+      this.isFetching.set(true);
+
+     const httpGetSub = this.placesService.loadAvailablePlaces()
+     .subscribe({
+      next: (places) => {
+        this.places.set(places);
+      },
+      error: (error: Error) => {
+        this.httpError.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      }
+     });
+
+     this.destroyRef.onDestroy(() => {
+      httpGetSub.unsubscribe();
+     })
+  }
+
+  onSelectPlace(selectedPlace: Place){
+    const subscription = this.placesService.addPlaceToUserPlaces(selectedPlace)
+    .subscribe({
+      next: (resData) => console.log(resData)
+    });;
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+     })
+  }
+
+
+
+
+
+}

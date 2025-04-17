1645792d4771b256e7a086e16f2916862e567201
mateusz.skonieczny <mateusz.skonieczny@EWM402>
2025-04-17 16:24:00 +0200

    Completed Module 14

--- Module_14/01-starting-project/src/app/app.routes.ts
+++ Module_14/01-starting-project/src/app/app.routes.ts
@@ -1,29 +1,42 @@
-import { Routes } from "@angular/router";
+import { CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 
+import { routes as userRoutes } from "./users/users.routes"
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { resolveTitle, resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segments) =>{
+    const router = inject(Router);
+    var shouldGetAccess = Math.random();
+
+    if (shouldGetAccess) {
+        return true;
+    }
+
+    return new RedirectCommand(router.parseUrl("/unauthorized"));
+}
 
 export const routes: Routes = [
     {
         path: "",
-        component: NoTaskComponent
+        component: NoTaskComponent,
+        title: "No task selected"
     },
     {
         path: "users/:userId",
         component: UserTasksComponent,
-        children: [
-            {
-                path: "tasks",
-                component: TasksComponent
-            },
-            {
-                path: "tasks/new",
-                component: NewTaskComponent
-            }
-        ]
+        children: userRoutes,
+        canMatch: [dummyCanMatch],
+        data: {
+            message: "Hello!"
+        },
+        resolve: {
+            userName: resolveUserName
+        },
+        title: resolveTitle
     },
     {
         path: "**",
--- Module_14/01-starting-project/src/app/users/user-tasks/user-tasks.component.ts
+++ Module_14/01-starting-project/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -12,7 +12,8 @@ import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 export class UserTasksComponent implements OnInit {
   //userId = input.required<string>();
 
-  userName = "";
+  userName = input.required<string>();
+  message = input.required<string>();
 
   private usersService = inject(UsersService);
   private activetedRoute = inject(ActivatedRoute);
@@ -23,13 +24,44 @@ export class UserTasksComponent implements OnInit {
   ); */
 
   ngOnInit(): void {
-    console.log(this.activetedRoute);
-    const subscription = this.activetedRoute.paramMap.subscribe({
+    // This way you can also get route values, but this code won't be executed if the value changes.
+    // NgOnInit will run only once.
+    //console.log(this.activetedRoute.snapshot.paramMap.get("userId"));
+
+    console.log(this.message());
+    
+
+    /* const subscription = this.activetedRoute.paramMap.subscribe({
       next: paramMap => { 
         this.userName = this.usersService.users.find(u => u.id === paramMap.get("userId"))?.name || "";
       }
     });
 
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+    this.destroyRef.onDestroy(() => subscription.unsubscribe()); */
+
+    // data from route can be also read in the component directly:
+    /* this.activetedRoute.data.subscribe({
+      next: data => {
+        console.log(data);
+      }
+    }) */
   }
+}
+
+// Function used in app.routes.ts. Dynamicaly returns a values for userName input in the user-tasks component:
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot, 
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName = usersService.users.find(u => u.id === activatedRoute.paramMap.get("userId"))?.name || "";
+
+  return userName;
+}
+
+export const resolveTitle: ResolveFn<string>  = (
+  activatedRoute: ActivatedRouteSnapshot, 
+  routerState: RouterStateSnapshot
+) => {
+  return resolveUserName(activatedRoute, routerState) + '\'s Tasks';
 }
